42811de19be5ef01b260d361bfa92b2c27e48ea5
Shinji <tanafamy_194@hotmail.com>
2021-01-11 12:12:31 +0900

    商品一覧表示機能実装の微修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,25 +34,50 @@ RSpec.describe Item, type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include('Category must be other than 1')
         end
+        it '商品のカテゴリーが空だと出品できない' do
+          @item.category_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank")
+        end
         it '商品の状態を選択しないと出品できない' do
           @item.status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Status must be other than 1')
+        end
+        it '商品の状態が空だと出品できない' do
+          @item.status_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Status can't be blank")
         end
         it '商品の配送料の負担を選択しないと出品できない' do
           @item.delivery_pay_method_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Delivery pay method must be other than 1')
         end
+        it '商品の配送料の負担が空だと出品できない' do
+          @item.delivery_pay_method_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Delivery pay method can't be blank")
+        end
         it '商品の発送元の地域を選択しないと出品できない' do
           @item.sender_region_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Sender region must be other than 1')
         end
+        it '商品の発送元の地域が空だと出品できない' do
+          @item.sender_region_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Sender region can't be blank")
+        end
         it '商品の発送までの日数を選択しないと出品できない' do
           @item.delivery_duration_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include('Delivery duration must be other than 1')
+        end
+        it '商品の発送までの日数が空だと出品できない' do
+          @item.delivery_duration_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Delivery duration can't be blank")
         end
         it '商品の価格を入力しないと出品できない' do
           @item.price = nil
@@ -71,6 +96,16 @@ RSpec.describe Item, type: :model do
         end
         it '商品の価格は半角数字以外だと出品できない' do
           @item.price = '１０００'
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price is not a number')
+        end
+        it '商品の価格は半角英語だけだと出品できない' do
+          @item.price = 'abcde'
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price is not a number')
+        end
+        it '商品の価格は半角英数混合だと出品できない' do
+          @item.price = 'abc123'
           @item.valid?
           expect(@item.errors.full_messages).to include('Price is not a number')
         end
