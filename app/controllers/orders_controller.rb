c1dd10b4967a4d00e04fc0132e02d91793ce21fd
Shinji <tanafamy_194@hotmail.com>
2021-01-15 17:47:58 +0900

    商品購入機能修正1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @order_delivery_address = OrderDeliveryAddress.new
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id || Order.exists?(item_id: @item.id)
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || Order.exists?(item_id: @item.id)
   end
 
   def create
@@ -24,15 +22,17 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery_address).permit(:postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_delivery_address).permit(:postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- spec/models/order_delivery_address_spec.rb
+++ spec/models/order_delivery_address_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe OrderDeliveryAddress, type: :model do
-    before do
-      @order_delivery_address = FactoryBot.build(:order_delivery_address)
-    end
+  before do
+    @order_delivery_address = FactoryBot.build(:order_delivery_address)
+  end
 
   describe '商品の購入' do
     context '商品の購入ができる場合' do
@@ -13,24 +13,24 @@ RSpec.describe OrderDeliveryAddress, type: :model do
 
     context '商品の購入ができない場合' do
       it '郵便番号を入力しないと購入できない' do
-        @order_delivery_address.postal_code = ""
+        @order_delivery_address.postal_code = ''
         @order_delivery_address.valid?
         expect(@order_delivery_address.errors.full_messages).to include("Postal code can't be blank")
       end
       it '郵便番号にハイフンを含めないと購入できない' do
-        @order_delivery_address.postal_code = "2380048"
+        @order_delivery_address.postal_code = '2380048'
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters")
+        expect(@order_delivery_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters')
       end
       it '郵便番号の前半部分が３文字出ないと購入できない' do
-        @order_delivery_address.postal_code = "22-0048"
+        @order_delivery_address.postal_code = '22-0048'
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters")
+        expect(@order_delivery_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters')
       end
       it '郵便番号の後半部分が４文字出ないと購入できない' do
-        @order_delivery_address.postal_code = "238-048"
+        @order_delivery_address.postal_code = '238-048'
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters")
+        expect(@order_delivery_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-) and fill in with 3 leters - 4 letters')
       end
       it '都道府県を入力しないと購入できない' do
         @order_delivery_address.prefecture_id = nil
@@ -40,35 +40,35 @@ RSpec.describe OrderDeliveryAddress, type: :model do
       it '都道府県の選択肢で「--」以外を選択しないと購入できない' do
         @order_delivery_address.prefecture_id = 0
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Prefecture must be other than 0")
+        expect(@order_delivery_address.errors.full_messages).to include('Prefecture must be other than 0')
       end
       it '市区町村を入力しないと購入できない' do
-        @order_delivery_address.city_town = ""
+        @order_delivery_address.city_town = ''
         @order_delivery_address.valid?
         expect(@order_delivery_address.errors.full_messages).to include("City town can't be blank")
       end
       it '番地を入力しないと購入できない' do
-        @order_delivery_address.street_number = ""
+        @order_delivery_address.street_number = ''
         @order_delivery_address.valid?
         expect(@order_delivery_address.errors.full_messages).to include("Street number can't be blank")
       end
       it '電話番号を入力しないと購入できない' do
-        @order_delivery_address.phone_number = ""
+        @order_delivery_address.phone_number = ''
         @order_delivery_address.valid?
         expect(@order_delivery_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it '電話番号にハイフンを含めると購入できない' do
-        @order_delivery_address.phone_number = "090-222-811"
+        @order_delivery_address.phone_number = '090-222-811'
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Phone number is invalid. Exclude hyphen(-) and make it within 11 numbers")
+        expect(@order_delivery_address.errors.full_messages).to include('Phone number is invalid. Exclude hyphen(-) and make it within 11 numbers')
       end
       it '電話番号が11文字より多いと購入できない' do
-        @order_delivery_address.phone_number = "090111111111"
+        @order_delivery_address.phone_number = '090111111111'
         @order_delivery_address.valid?
-        expect(@order_delivery_address.errors.full_messages).to include("Phone number is invalid. Exclude hyphen(-) and make it within 11 numbers")
+        expect(@order_delivery_address.errors.full_messages).to include('Phone number is invalid. Exclude hyphen(-) and make it within 11 numbers')
       end
       it 'クレジットカード情報を正しく入力しないと購入できない' do
-        @order_delivery_address.token = ""
+        @order_delivery_address.token = ''
         @order_delivery_address.valid?
         expect(@order_delivery_address.errors.full_messages).to include("Token can't be blank")
       end
