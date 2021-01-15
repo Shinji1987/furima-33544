6ccec1f69c5868fca0c7d7f4c69cbc274c805ba1
Shinji <tanafamy_194@hotmail.com>
2021-01-15 11:17:50 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_delivery_address = OrderDeliveryAddress.new(order_params)
     if @order_delivery_address.valid?
+      pay_item
       @order_delivery_address.save
       redirect_to root_path
     else
@@ -23,6 +24,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery_address).permit(:postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery_address).permit(:postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -1,13 +1,14 @@
 class OrderDeliveryAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city_town, :street_number, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city_town
     validates :street_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: "is invalid. Exclude hyphen(-)" }
+    validates :token
   end
 
   # validates :prefecture_id, numericality: { other_than: 0 }
